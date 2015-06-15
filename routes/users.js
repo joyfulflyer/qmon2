4d585e7d14b0b1f0a4cd02c46fbecae1f10a7000
joyfulflyer <joyfulflyer@gmail.com>
2015-06-14 19:43:59 -0700

    passing basic api tests

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var dbSession = require('../src/dbsession.js');
+var dbSession = require('../src/dbSession.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	dbSession.fetchAll('SELECT * FROM users', function(err, rows) {
+		if (err) {
+			//console.log(err);
+			err.status = 500;
+			next(err);
+		} else {
+			console.log(rows);
+			res.json(rows);
+		}
+	});
+//  res.send('respond with a resource');
+});
+
+router.post('/' function(req, res, next) {
+	console.log('poast');
 });
 
 module.exports = router;
--- spec/usersApiSpec.js
+++ spec/usersApiSpec.js
@@ -11,20 +11,14 @@ describe("The users API", function () {
 	var server;
 	var port = 8081;
 	
-	var expected = {
-		"_users": [
-			{'id': 1, 'name': 'Bob', 'type': 1},
-			{'id': 2, 'name': 'Sally', 'type': 2, 'call_start': '20'},
-			{'id': 3, 'name': 'Lee', 'type': 2}
-		]
-	}
+
 	
 	beforeEach(function(done) {
 		app.set('port', port);
 		server = http.createServer(app);
 		server.listen(port, function (err) {
 			console.log('started listening on port: ' + port);
-			console.log('reset database?');
+			resetDatabase(dbSession, done);
 			done(err);
 		});
 	});
@@ -32,53 +26,76 @@ describe("The users API", function () {
 	afterEach(function(done) {
 		server.close(function() {
 			console.log('closing server');
+			resetDatabase(dbSession, done);
 			done();
 		});
 	});
 	
-	it('should respond to GET at /', function(done) {
+	it('should respond to GET at /users/', function(done) {
+		
+		var expected = [
+		{'id': 1, 'name': 'Bob', 'type': 1, 'call_start': null},
+		{'id': 2, 'name': 'Sally', 'type': 2, 'call_start': '20'},
+		{'id': 3, 'name': 'Lee', 'type': 2, 'call_start': null}
+		];
+		
 		
 		async.series( 
 			[
 				function(callback) {
+					debugger;
+					console.log('doing first insert');
 					dbSession.insert(
 						'users',
 						{'name': 'Bob', 'type': 1},
-						function(err) {callback(err)}
+						function(err) {
+							console.log ('error: '+ err)
+							
+							callback(err)}
 					);
+					console.log('\n\n' + dbSession);
 				},
 				
 				function(callback) {
+					console.log('second insert');
 					dbSession.insert(
 						'users',
 						{'name': 'Sally', 'type': 2, 'call_start': '20'},
 						function(err) { callback(err) }
 					);
+					console.log('did second insert');
 				},
 				
 				function(callback) {
+					console.log('third insert');
 					dbSession.insert(
 						'users',
 						{'name': 'Lee', 'type': 2},
 						function(err) {callback(err) }
 					);
 				}
-			]
-		)
+			], function(err, results) {
+				
+				request.get(
+					{
+						'url': 'http://localhost:8081/users/',
+						'json': true
+					},
+					function (err, response, body) {
+						console.log(err);
+						console.log(response);
+						console.log(body);
+						expect(response.statusCode).toBe(200);
+						expect(body).toEqual(expected);
+						done();
+					}
+				);
+				
+			}
+		);
 		
 		
 		
-		request.get(
-			{
-				'url': 'http://localhost:8081/',
-				'json': true
-			},
-			function (err, response, body) {
-				console.log(err);
-				console.log(response);
-				console.log(body);
-				expect(response.statusCode).toBe(200);
-				done();
-			});
+		
 	});	
 });
