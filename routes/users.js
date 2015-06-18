6b8dd4d559b2182be37074cd3980c9cdef4b1f97
joyfulflyer <joyfulflyer@gmail.com>
2015-06-18 07:35:29 -0700

    more experiments

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/' function(req, res, next) {
+router.post('/', function(req, res, next) {
 	console.log('poast' + req.body);
 });
 
