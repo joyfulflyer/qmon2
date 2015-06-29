c0e2fbc9e3218f648aaba92290be3d95a0c61103
joyfulflyer <joyfulflyer@gmail.com>
2015-06-28 23:09:10 -0700

    non working zendesk wrapper

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var app = require('./app.js');
 var http = require('http');
 var zdWrapper = require('./src/zdWrapper.js');
+var arguments = process.argv.slice(2);
 
 
 var server = http.createServer(app);
@@ -11,4 +12,10 @@ server.listen(8080, function() {
 	console.log('started listening on port 8080');
 })
 
-zdWrapper.connect()
+zdWrapper.connect(arguments[0], arguments[1], arguments[3], function(err) {
+	console.log(err);
+})
+
+zdWrapper.getQueuStatus(function (res) {
+	console.log(res);
+})
--- src/zdWrapper.js
+++ src/zdWrapper.js
@@ -7,21 +7,23 @@ var http = require('http');
 
 
 var zdWrapper;
-
-zdWrapper.connect = function(org, user, key, callback) {
+//maybe I'm too java but I think I want a zdWrapper object to carry around, is that what i'm doing?
+var zdWrapper.connect = function(org, user, key, callback) {
 	if (user === 'undefined' || org === 'undefined' || key === 'undefined') {
-		callback('Need orginization, username and API key to be able to connect'); 
+		throw new Error('Need orginization, username and API key to be able to connect'); 
 	}
 	
 	var basePath = '/api/v2';
 	var hostname = 'https://' + org + '.zendesk.com';
 	var auth = ':' + user + '/token:' + key; //I hope this is right
-};
+	console.log('set some variables');
+	callback();
+}
 
-zdWrapper.getQueueStatus(callback) {
+var zdWrapper.getQueueStatus = function(callback) {
 	var options = {
-		hostname = hostname,
-		auth = auth,
+		hostname: hostname,
+		auth: auth,
 		path: basePath + '/channels/voice/stats/current_queue_activity'
 	}
 	http.get(options, callback);
