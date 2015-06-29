6757836b90312d007b04315ab79373334f5aa674
Ben McKinley <bmckinley@groupon.com>
2015-06-29 14:15:23 -0700

    connect to zd

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 'use strict';
 
-var app = require('./app.js');
+//var app = require('./app.js');
 var http = require('http');
 var zdWrapper = require('./src/zdWrapper.js');
-var arguments = process.argv.slice(2);
+var args = process.argv.slice(2);
 
-
+/*
 var server = http.createServer(app);
 
 server.listen(8080, function() {
 	console.log('started listening on port 8080');
 })
 
-zdWrapper.connect(arguments[0], arguments[1], arguments[3], function(err) {
+*/
+
+zdWrapper.connect(args[0], args[1], args[2], function(err) {
 	console.log(err);
 })
 
-zdWrapper.getQueuStatus(function (res) {
-	console.log(res);
+var str = '';
+
+zdWrapper.getQueueStatus(function (res) {
+	res.on('data', function(chunk) {
+		console.log('got chunk');
+		console.log(chunk);
+		str += chunk;
+	})
+	res.on('end', function () {
+		console.log('end');
+		console.log('message: ' + str);
+
+	})
+	console.log('RESPONSE');
+	console.log(res.statusCode);
+//	console.log(res);
+	console.log("end response");
 })
--- src/zdWrapper.js
+++ src/zdWrapper.js
@@ -3,30 +3,41 @@
 var express = require('express');
 var dbSession = require('../src/dbSession.js');
 var bodyParser = require('body-parser');
-var http = require('http');
+var https = require('https');
 
 
-var zdWrapper;
+var zdWrapper = {};
 //maybe I'm too java but I think I want a zdWrapper object to carry around, is that what i'm doing?
-var zdWrapper.connect = function(org, user, key, callback) {
+zdWrapper.connect = function(org, user, key, callback) {
 	if (user === 'undefined' || org === 'undefined' || key === 'undefined') {
 		throw new Error('Need orginization, username and API key to be able to connect'); 
 	}
 	
-	var basePath = '/api/v2';
-	var hostname = 'https://' + org + '.zendesk.com';
-	var auth = ':' + user + '/token:' + key; //I hope this is right
+	zdWrapper.basePath = '/api/v2';
+	zdWrapper.hostname = org + '.zendesk.com';
+	zdWrapper.auth = user + '/token:' + key; //I hope this is right
+	zdWrapper.headers = {
+		Accept: 'application/json'
+	}
 	console.log('set some variables');
+	console.log(zdWrapper);
 	callback();
 }
 
-var zdWrapper.getQueueStatus = function(callback) {
+zdWrapper.getQueueStatus = function(callback) {
 	var options = {
-		hostname: hostname,
-		auth: auth,
-		path: basePath + '/channels/voice/stats/current_queue_activity'
+		hostname: zdWrapper.hostname,
+		auth: zdWrapper.auth,
+		path: zdWrapper.basePath + '/channels/voice/stats/current_queue_activity',
+		headers: zdWrapper.headers
 	}
-	http.get(options, callback);
+	console.log(options);
+	https.get(options, callback).on('error', function(e) {
+		console.log('Got error: ' + e.message);
+
+	});
 }
+
+
 
 module.exports = zdWrapper;
