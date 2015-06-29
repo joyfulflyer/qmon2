4420b76b8e901e1ca1521997496f0b409b7f901b
joyfulflyer <joyfulflyer@gmail.com>
2015-06-28 23:11:09 -0700

    note on what you need

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ server.listen(8080, function() {
 	console.log('started listening on port 8080');
 })
 
+// [0].zendesk.com, username, api key
 zdWrapper.connect(arguments[0], arguments[1], arguments[3], function(err) {
 	console.log(err);
 })
