f662ce57d88e4b6e879dfe2f645097f4d7475095
Ben McKinley <bmckinley@groupon.com>
2015-06-30 18:33:44 -0700

    Get the agent activity

--- src/zdWrapper.js
+++ src/zdWrapper.js
@@ -5,6 +5,7 @@ var dbSession = require('../src/dbSession.js');
 var bodyParser = require('body-parser');
 var https = require('https');
 var callQueue = require('../models/callQueue');
+var async = require('async');
 
 var zdWrapper = {};
 //maybe I'm too java but I think I want a zdWrapper object to carry around, is that what i'm doing?
@@ -22,7 +23,9 @@ zdWrapper.connect = function(org, user, key, callback) {
 //	console.log('set some variables');
 	console.log(zdWrapper);
 	console.log(this);
-	startPollingQueueStatus();
+//	startPollingQueueStatus();
+//	startPollingUserStatus();
+	getUserStatus(0);
 	callback();
 }
 
@@ -60,20 +63,135 @@ var startPollingQueueStatus = function() {
 	var queuePoll = setInterval(poll, 1000); // to shut it off
 }
 
-/*
-var startPollingUserStatus = function() {
-	for (i = 0; )
+var allContent = [];
 
+// Current page is designed to start at 0. It uses currentPage + 1 to determine the page to view
+var getUserStatus = function(currentPage) {
+	var cont = true;
+	var i = 0;
 
 	var options = {
 		hostname: zdWrapper.hostname,
 		auth: zdWrapper.auth,
-		path: zdWrapper.basePath + '/channels/voice/stats/current_queue_activity',
+		path: zdWrapper.basePath + '/channels/voice/stats/agents_activity.json?page=' + (currentPage + 1),
 		headers: zdWrapper.headers
-
 	};
+
+	https.get(options, function(response) {
+		var content = "";
+		response.on('data', function(chunk) {
+			content += chunk;
+		});
+		response.on('end', function() {
+			console.log ('content ' + (currentPage + 1));
+			content = JSON.parse(content);
+//			console.log(content);
+			if (content.next_page != null) {
+				allContent = allContent.concat(content.agents_activity);
+//				allContent.push(content.agents_activity);
+				console.log('read page ' + (currentPage + 1));
+				getUserStatus(currentPage + 1);
+			} else {
+				console.log('no more pages');
+				console.log(allContent);
+				console.log('printed all content');
+			}
+		});
+
+
+	});
+
 }
+/*
+	async.whilst(function() {
+		console.log('cont ' + cont);
+		return cont;
+	}, function() {
+		
+
+
+		https.get(options, function(response) {
+			var content = "";
+			response.on('data', function(chunk) {
+//				console.log('got chunk');
+				content += chunk;
+			});
+			response.on('end', function() {
+				content = JSON.parse(content);
+				console.log(content.next_page);
+				if (content.next_page === 'undefined') {
+					console.log('found undefined');
+//					console.log(content.next_page);
+					cont = false;
+				} else {
+					allContent += content;
+				}
+				console.log('end');
+
+
+			//	console.log(content);
+			});
+			console.log('get finished');
+		});
+
+		console.log('moving on i=' + i);
+		console.log('continue?: ' + cont);
+
+		i++;
+	}, function(err) {
+		if (err) {
+			console.log('got error: ' + error);
+		} else {
+			console.log('end');
+			console.log(allContent);
+		}
+
+	});
 */
+
+/*
+	for (var i = 1; i < 5; i++) {
+
+		var options = {
+			hostname: zdWrapper.hostname,
+			auth: zdWrapper.auth,
+			path: zdWrapper.basePath + '/channels/voice/stats/agents_activity.json?page=' + i,
+			headers: zdWrapper.headers
+		};
+
+
+		https.get(options, function(response) {
+			var content = "";
+			response.on('data', function(chunk) {
+				console.log('got chunk');
+				content += chunk;
+			});
+			response.on('end', function() {
+				content = JSON.parse(content);
+				console.log(content.next_page);
+				if (content.next_page === 'undefined') {
+					console.log('found undefined');
+					console.log(content.next_page);
+					cont = false;
+				} else {
+					allContent += content;
+				}
+
+
+			//	console.log(content);
+			});
+		});
+
+	}
+	console.log(allContent);
+
+*/
+
+	
+
+
+
+
 zdWrapper.getQueueStatus = function() {
 	// another way of getting data out
 	return queueStatus;
